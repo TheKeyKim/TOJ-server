d9f9ae2c7ace4acc85a182eae2c9f070e10a3d02
TheKey <df1097@naver.com>
2021-04-10 18:34:36 +0900

    compile test

--- routes/problem.js
+++ routes/problem.js
@@ -5,6 +5,27 @@ const db = require('../models/index')
 const {verifyToken} = require('../utils/jwt');
 const child_process = require('child_process');
 
+async function solving(language, code, problem_id, submit_id){ 
+    const ext = [".cpp", ".java", ".py"];
+    var dir = `./scoring/source/`
+    var input = `./scoring/input/${problem_id}`
+    var output = `./scoring/output/${submit_id}/`
+
+    var cmd = "";
+    cmd += `echo "${code}" > ${dir}${submit_id}${ext[language]}\n`
+    cmd += `mkdir ${output}\n`
+    if(language == 0){
+        var tmp = `g++ ${dir}${submit_id}${ext[language]} -o ${submit_id} && ./${submit_id} `;
+
+    }else if(language == 1){
+        // var tmp = `javac ${dir}${submit_id}${ext[language]} -o ${submit_id} && java `
+    }else if(language == 2){
+        var tmp = `python3 ${dir}${submit_id}${ext[language]}`
+        console.log(cmd)
+        console.log(tmp)
+    }
+}
+
 // get data POST
 router.post('/submit', verifyToken, async (req, res) => {
     try{
@@ -17,22 +38,24 @@ router.post('/submit', verifyToken, async (req, res) => {
             }
         })
 
-        var code = req.body.code;
-        console.log(code);
+        const {language, code, id, problem_id} = req.body;
+        console.log(language);
+        solving(language, code, problem_id, id);
+
         var cmd = "echo \"" + code + "\" > " + dir + String(req.body.id) + ".py";
         var cmds = `
             echo "${code}" > ${dir+req.body.id}.py
             python3 ${dir+req.body.id}.py < ${input}${req.body.problem_id}/0.txt > ${output}${req.body.problem_id}/0.txt
         `
-        console.log(cmds);
-        child_process.exec(cmds, function(err,stdout,stderr){
-            if(err){
-                console.log('child process exited with error code', err.code);
-                return ;
-            }else{
-                console.log(stdout);
-            }
-        })
+        // console.log(cmds);
+        // child_process.exec(cmds, function(err,stdout,stderr){
+        //     if(err){
+        //         console.log('child process exited with error code', err.code);
+        //         return ;
+        //     }else{
+        //         console.log(stdout);
+        //     }
+        // })
         return res.json({
             status:200,
             name:data["name"],
